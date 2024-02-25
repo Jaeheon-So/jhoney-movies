8d43e1f1b3830d6406a001a15bf75ac5acfa937f
Jaeheon-So <sjh-98@naver.com>
2024-02-26 00:40:40 +0900

    feat: 메인 페이지 movie card 날짜 형식 변경

--- src/app/_components/MovieCard.tsx
+++ src/app/_components/MovieCard.tsx
@@ -5,6 +5,7 @@ import { IMovieInfo } from "@/model/movie";
 import Link from "next/link";
 import Image from "next/image";
 import styles2 from "./fadeInOut.module.scss";
+import { FormatMDY } from "../_utils/dayFormat";
 
 type Props = {
   movie: IMovieInfo;
@@ -28,7 +29,7 @@ const MovieCard = ({ movie }: Props) => {
       </div>
       <div className={styles.content}>
         <div className={styles.title}>{movie.title}</div>
-        <div className={styles.date}>{"Apr 05, 2015"}</div>
+        <div className={styles.date}>{FormatMDY(movie.release_date)}</div>
       </div>
     </Link>
   );
