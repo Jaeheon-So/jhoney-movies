f423d9881ee1bb2e19032ab7eea0b88978187589
Jaeheon-So <sjh-98@naver.com>
2024-02-29 23:44:33 +0900

    feat: tv top rated  페이지 api 연결

---
+++ src/app/(routes)/tv/top-rated/_component/DiscoverTTv.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import useInfiniteScroll from "@/app/_hooks/useInfiniteScroll";
+import { getDiscoverTTv } from "@/app/_lib/getDiscoverTTv";
+import { PopularTvInfo } from "@/model/Movie";
+import React, { Fragment } from "react";
+import DiscoverTvCard from "../../_component/DiscoverTvCard";
+import styles from "./discoverTTv.module.scss";
+
+type Props = {
+  searchParams: { sort_by?: string; with_genres?: string };
+};
+
+const DiscoverTTv = ({ searchParams }: Props) => {
+  const { ref, data, isFetching, isLoading } = useInfiniteScroll(
+    ["movies", "top-rated", "tv", searchParams],
+    getDiscoverTTv
+  );
+
+  return (
+    <>
+      <div className={styles.container}>
+        {data?.pages.map((page, index) => (
+          <Fragment key={index}>
+            {page.results.map((movie: PopularTvInfo, index: number) => (
+              <DiscoverTvCard key={index} movie={movie} />
+            ))}
+          </Fragment>
+        ))}
+      </div>
+      <div ref={ref} className={styles.loading}>
+        {isFetching || isLoading ? <div>Loading...</div> : null}
+      </div>
+    </>
+  );
+};
+
+export default DiscoverTTv;
--- src/app/(routes)/tv/top-rated/page.tsx
+++ src/app/(routes)/tv/top-rated/page.tsx
@@ -1,7 +1,30 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 import React from "react";
+import DiscoverTTv from "./_component/DiscoverTTv";
+import { getDiscoverTTv } from "@/app/_lib/getDiscoverTTv";
 
-const TopRatedTvPage = () => {
-  return <div>TopRatedTvPage</div>;
+type Props = {
+  searchParams: { sort_by?: string; with_genres?: string };
+};
+
+const TopRatedTvPage = async ({ searchParams }: Props) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["movies", "top-rated", "tv", searchParams],
+    queryFn: getDiscoverTTv,
+    initialPageParam: 1,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <DiscoverTTv searchParams={searchParams} />;
+    </HydrationBoundary>
+  );
 };
 
 export default TopRatedTvPage;
