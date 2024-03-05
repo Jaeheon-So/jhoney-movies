43b5b03898540da4692df6a19062f1130fbf2b18
Jaeheon-So <sjh-98@naver.com>
2024-03-05 15:37:45 +0900

    feat: Tv page 에러 다시시도 추가

--- src/app/(routes)/tv/_component/DiscoverPTv.tsx
+++ src/app/(routes)/tv/_component/DiscoverPTv.tsx
@@ -7,27 +7,33 @@ import React, { Fragment } from "react";
 import styles from "./discoverPTv.module.scss";
 import DiscoverTvCard from "./DiscoverTvCard";
 import LoadingCircle from "@/app/_components/LoadingCircle";
+import HomeError from "@/app/_components/HomeError";
 
 type Props = {
   searchParams: { sort_by?: string; with_genres?: string };
 };
 
 const DiscoverPTv = ({ searchParams }: Props) => {
-  const { ref, data, isFetching, isLoading } = useInfiniteScroll(
-    ["movies", "popular", "tv", searchParams],
-    getDiscoverPTv
-  );
+  const { ref, data, isFetching, isLoading, isError, refetch } =
+    useInfiniteScroll(
+      ["movies", "popular", "tv", searchParams],
+      getDiscoverPTv
+    );
 
   return (
     <>
       <div className={styles.container}>
-        {data?.pages.map((page, index) => (
-          <Fragment key={index}>
-            {page.results.map((movie: PopularTvInfo, index: number) => (
-              <DiscoverTvCard key={index} movie={movie} />
-            ))}
-          </Fragment>
-        ))}
+        {isError ? (
+          <HomeError message="검색 오류" refetch={refetch} />
+        ) : (
+          data?.pages.map((page, index) => (
+            <Fragment key={index}>
+              {page.results.map((movie: PopularTvInfo, index: number) => (
+                <DiscoverTvCard key={index} movie={movie} />
+              ))}
+            </Fragment>
+          ))
+        )}
       </div>
       <div ref={ref} className={styles.loading}>
         {isFetching || isLoading ? <LoadingCircle /> : null}
