9a237008a9e2586a0455f93cc74f62ce8e1b79a5
Jaeheon-So <sjh-98@naver.com>
2024-03-05 15:39:13 +0900

    feat: people page 에러 다시시도 추가

--- src/app/(routes)/people/_component/PopularPeopleList.tsx
+++ src/app/(routes)/people/_component/PopularPeopleList.tsx
@@ -7,25 +7,28 @@ import { getPopularPeople } from "@/app/_lib/getPopularPeople";
 import LoadingCircle from "@/app/_components/LoadingCircle";
 import { PopularPeopleInfo } from "@/model/People";
 import PopularPeopleCard from "./PopularPeopleCard";
+import HomeError from "@/app/_components/HomeError";
 
 type Props = {};
 
 const PopularPeopleList = ({}: Props) => {
-  const { ref, data, isFetching, isLoading } = useInfiniteScroll(
-    ["people", "popular"],
-    getPopularPeople
-  );
+  const { ref, data, isFetching, isLoading, isError, refetch } =
+    useInfiniteScroll(["people", "popular"], getPopularPeople);
 
   return (
     <>
       <div className={styles.container}>
-        {data?.pages.map((page, index) => (
-          <Fragment key={index}>
-            {page.results.map((people: PopularPeopleInfo, index: number) => (
-              <PopularPeopleCard key={index} people={people} />
-            ))}
-          </Fragment>
-        ))}
+        {isError ? (
+          <HomeError message="검색 오류" refetch={refetch} />
+        ) : (
+          data?.pages.map((page, index) => (
+            <Fragment key={index}>
+              {page.results.map((people: PopularPeopleInfo, index: number) => (
+                <PopularPeopleCard key={index} people={people} />
+              ))}
+            </Fragment>
+          ))
+        )}
       </div>
       <div ref={ref} className={styles.loading}>
         {isFetching || isLoading ? <LoadingCircle /> : null}
