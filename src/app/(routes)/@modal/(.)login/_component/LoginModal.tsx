93677132bcad7806df0b24012a396a65af74832c
Jaeheon-So <sjh-98@naver.com>
2024-03-13 03:25:55 +0900

    feat: 로그인 모달 페이지에서 회원가입 페이지 이동 시 모달 안닫히는 에러 해결

--- src/app/(routes)/@modal/(.)login/_component/LoginModal.tsx
+++ src/app/(routes)/@modal/(.)login/_component/LoginModal.tsx
@@ -2,7 +2,7 @@
 
 import React, { ReactNode, useRef } from "react";
 import styles from "../page.module.scss";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { IoClose } from "react-icons/io5";
 
 type Props = {
@@ -12,6 +12,7 @@ type Props = {
 const LoginModal = ({ children }: Props) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
+  const pathname = usePathname();
 
   const modalOutSideClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (modalRef.current === e.target) {
@@ -22,6 +23,10 @@ const LoginModal = ({ children }: Props) => {
   const onClickClose = () => {
     router.back();
   };
+
+  if (pathname !== "/login") {
+    return null;
+  }
 
   return (
     <div className={styles.overlay} ref={modalRef} onClick={modalOutSideClick}>
