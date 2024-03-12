7de27cce82e6567f68ff0e761597dc7f281c17f1
Jaeheon-So <sjh-98@naver.com>
2024-03-13 03:20:10 +0900

    feat: 로그인 페이지 parallel route에 meta data 추가

--- src/app/(routes)/@modal/(.)login/page.tsx
+++ src/app/(routes)/@modal/(.)login/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import LoginForm from "../../login/_component/LoginForm";
 import LoginModal from "./_component/LoginModal";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "로그인 | HONEY-BOX",
+  description: "로그인하여 마음에 드는 영화를 저장해보세요.",
+};
 
 const InterceptLoginPage = () => {
   return (
