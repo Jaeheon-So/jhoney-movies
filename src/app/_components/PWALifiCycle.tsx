a1e7573dfeaa89244e70438797fc84043fe12de5
Jaeheon-So <sjh-98@naver.com>
2024-04-16 22:34:49 +0900

    feat: pwa 업데이트 여부 확인하는 테스트 커밋6

--- src/app/_components/PWALifiCycle.tsx
+++ src/app/_components/PWALifiCycle.tsx
@@ -56,29 +56,32 @@ export function PWALifeCycle() {
         console.log(event);
       });
 
-      // wb.addEventListener("waiting", () => {
-      // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
-      // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
-      // You may want to customize the UI prompt accordingly.
-      // https://developer.chrome.com/docs/workbox/handling-service-worker-updates/#the-code-to-put-in-your-page
-      // if (
-      //   confirm(
-      //     "A newer version of this web app is available, reload to update?"
-      //   )
-      // ) {
-      //   // Send a message to the waiting service worker, instructing it to activate.
-      //   // wb.messageSkipWaiting();
-      //   // wb.addEventListener("controlling", () => {
-      //   //   window.location.reload();
-      //   // });
-      // } else {
-      //   console.log(
-      //     "User rejected to update SW, keeping the old version. New version will be automatically loaded when the app is opened next time."
-      //   );
-      // }
-      // });
+      wb.addEventListener("waiting", () => {
+        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+        // You may want to customize the UI prompt accordingly.
+        // https://developer.chrome.com/docs/workbox/handling-service-worker-updates/#the-code-to-put-in-your-page
+        if (
+          confirm(
+            "A newer version of this web app is available, reload to update?"
+          )
+        ) {
+          // Send a message to the waiting service worker, instructing it to activate.
+          wb.messageSkipWaiting();
+          wb.addEventListener("controlling", () => {
+            window.location.reload();
+          });
+        } else {
+          alert(
+            "User rejected to update SW, keeping the old version. New version will be automatically loaded when the app is opened next time."
+          );
+          console.log(
+            "User rejected to update SW, keeping the old version. New version will be automatically loaded when the app is opened next time."
+          );
+        }
+      });
 
-      wb.addEventListener("waiting", () => checkUpdate(wb));
+      // wb.addEventListener("waiting", () => checkUpdate(wb));
 
       wb.addEventListener("controlling", (event: any) => {
         console.log(`Event ${event.type} is triggered.`);
